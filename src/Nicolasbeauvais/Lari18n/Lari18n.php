452cd77e9c4172f73d6b84b31d391e4eb82d3ea8
Nicolas Beauvais <n.beauvais78@gmail.com>
2015-01-15 08:59:27 +0100

    Refactor Lari18n::class

--- src/Nicolasbeauvais/Lari18n/Lari18n.php
+++ src/Nicolasbeauvais/Lari18n/Lari18n.php
@@ -27,12 +27,12 @@ class Lari18n
     /**
      * @var array
      */
-    private $paths= array();
+    private $paths= [];
 
     /**
      * @var Lari18n
      */
-    private $instance;
+    private static $instance = null;
 
     /**
      * @var string
@@ -42,17 +42,17 @@ class Lari18n
     /**
      * @var array
      */
-    private $languagesList = array();
+    private $languagesList = [];
 
     /**
      * @var array
      */
-    private $languagesData = array();
+    private $languagesData = [];
 
     /**
      * @var array
      */
-    private $languagesProgress = array();
+    private $languagesProgress = [];
 
     /**
      * @var bool
@@ -60,13 +60,15 @@ class Lari18n
     private static $activated = false;
 
     /**
-     * Construct.
+     * Construct lari18n.
      */
     private function __construct()
     {
         $this->app = app();
-        $this->paths['js'] = asset('packages/nicolasbeauvais/lari18n/js') . '/';
-        $this->paths['lang'] = app_path() . '/lang';
+        $this->paths = [
+            'js' => asset('packages/nicolasbeauvais/lari18n/js') . '/',
+            'lang' => app_path() . '/lang'
+        ];
     }
 
     /**
@@ -76,12 +78,11 @@ class Lari18n
      */
     public static function getInstance()
     {
-        static $instance = null;
-        if (null === $instance) {
-            $instance = new Lari18n();
+        if (self::$instance === null) {
+            self::$instance = new Lari18n();
         }
 
-        return $instance;
+        return self::$instance;
     }
 
     /**
@@ -93,7 +94,7 @@ class Lari18n
     }
 
     /**
-     * Wrap translated text with lari18n tags.
+     * Wrap translated text with lari18n tags and attributes.
      *
      * @param  mixed   $trans
      * @param  string  $key
@@ -105,41 +106,45 @@ class Lari18n
      */
     public function wrap($trans, $key, $replace, $locale, $number = null)
     {
-
+        // If an array is returned by the translation, we can't wrap it
         if (!is_string($trans)) {
             return $trans;
         }
 
-        $attributes = array();
+        // Store the tag attributes
+        $attributes = [];
 
-        if (starts_with($trans, $this->lang_key)) {
+        // If the translation start with the Lari18n key, it's a new one
+        $attributes['todo'] = starts_with($trans, $this->lang_key);
 
-            $attributes['todo'] = true;
-            $trans = str_replace($this->lang_key, '', $trans);
+        // Remove the special Lari18n key of the translation
+        $trans = str_replace($this->lang_key, '', $trans);
+
+        // If the translation is the key, the translation is missing
+        $attributes['missing'] = $trans == $key;
+
+        // Set Locale
+        $attributes['locale'] = $locale;
+
+        // For choice type translation we also store the given number
+        $attributes['number'] = $number ?: false;
+
+        // Add replace key/value to the tag's attributes
+        $attributes['replace'] = '';
+        foreach ($replace as $keyR => $valueR) {
+            $attributes['replace'] = $attributes['replace'] . $keyR . ':' . $valueR . ',';
         }
 
-        if ($trans == $key) {
-            $attributes['missing'] = true;
-        }
-
-        if (!empty($replace)) {
-
-            $attributes['replace'] = '';
-
-            foreach ($replace as $keyR => $valueR) {
-                $attributes['replace'] .= $keyR . ':' . $valueR . ',';
-            }
-        }
-
+        // Store fallback translation and key
         $attributes['origin'] = addslashes(Lang::get($key, $replace, Config::get('app.fallback_locale'), false));
         $attributes['key'] = $key;
 
-        if ($number) {
-            $attributes['number'] = $number;
-        }
-
         // Build attributes
         foreach ($attributes as $key => $attribute) {
+
+            // Empty/Null/False attribute aren't used
+            if (!$attributes) { continue; }
+
             $attributes[$key] = 'data-' . $key . '= "' . $attribute . '"';
         }
 
@@ -147,32 +152,23 @@ class Lari18n
     }
 
     /**
-     * Modify the laravel response if needed to add th toolbar.
+     * Modify the laravel response, if needed, to add the toolbar.
      *
      * @param  \Illuminate\Http\Request $request
-     * @param  \Symfony\Component\HttpFoundation\Response $response
+     * @param  \Illuminate\Http\Response $response
      *
-     * @return \Symfony\Component\HttpFoundation\Response
-
+     * @return \Illuminate\Http\Response
      * @return mixed
      */
     public function modifyResponse($request, $response)
     {
         $app = $this->app;
 
-        if ($app->runningInConsole()) {
+        if ($app->runningInConsole() || $request->isXmlHttpRequest() || $request->wantsJson()) {
             return $response;
-        }
-
-        if ($request->isXmlHttpRequest() || $request->wantsJson()) {
-
-            return $response;
-
         } elseif (($response->headers->has('Content-Type') &&
                 strpos($response->headers->get('Content-Type'), 'html') === false) || 'html' !== $request->format()) {
-
             return $response;
-
         }
 
         $this->injectToolbar($response);
@@ -192,48 +188,56 @@ class Lari18n
         $renderedContent = '<script type="text/javascript" src="' . $this->paths['js'] . 'lari18n.js'  . '"></script>';
 
         $pos = strripos($content, '</body>');
-        if (false !== $pos) {
+
+        if ($pos !== false) {
             $content = substr($content, 0, $pos) . $renderedContent . substr($content, $pos);
         } else {
             $content = $content . $renderedContent;
         }
+
         $response->setContent($content);
     }
 
+    /**
+     * Get the localisation languages list and set it to a class parameter.
+     *
+     */
     private function setLanguagesList()
     {
         $languagesList = File::directories($this->paths['lang']);
 
         foreach ($languagesList as $key => $language) {
-            $languagesList[$key] = substr(str_replace($this->paths['lang'], '', $language), 1);
+            $this->languagesList[$key] = substr(str_replace($this->paths['lang'], '', $language), 1);
         }
-
-        $this->languagesList = $languagesList;
     }
 
     /**
+     * Get all needed data of each localisation languages and set it to a class parameter.
+     *
      * @return array
      */
     private function setLanguagesData()
     {
         $languagesData = array_flip($this->languagesList);
 
+
+        // Open each file in the localisation directory and retrieve all the data of a localisation.
         foreach ($languagesData as $key => $value) {
             $files = File::allFiles($this->paths['lang'] . '/' . $key);
 
-            $languagesData[$key] = array();
+            $this->languagesData[$key] = [];
 
             foreach ($files as $fKey => $file) {
-                $fileName = str_replace(array('.php', '\\'), array('', '/'), $file->getRelativePathname());
+                $fileName = str_replace(['.php', '\\'], ['', '/'], $file->getRelativePathname());
 
-                $languagesData[$key][$fileName] = Lang::get($fileName, array(), $key);
+                $this->languagesData[$key][$fileName] = Lang::get($fileName, [], $key);
             }
 
-            $dot_array = array_dot($languagesData[$key]);
+            $dot_array = array_dot($this->languagesData[$key]);
             $count = count($dot_array);
 
+            // count the number of translated element in each file
             foreach ($dot_array as $item) {
-
                 if (starts_with($item, $this->lang_key)) {
                     $count--;
                 }
@@ -241,45 +245,47 @@ class Lari18n
 
             $this->languagesProgress[$key] = $count;
         }
-
-        $this->languagesData = $languagesData;
     }
 
     /**
      * Get all data needed for toolbar.
+     *
+     * @return array
      */
     public function getToolbarData()
     {
+        // Initialise lari18n data.
         $this->makeI18nData();
 
-        $data = array();
+        $data = [];
 
         // Get locales
         $data['locale'] = Config::get('app.locale');
         $data['fallback_locale'] = Config::get('app.fallback_locale');
 
-
+        // Get language information
         $data['languages'] = $this->languagesList;
         $data['languagesData'] = $this->languagesData;
         $data['languagesProgress'] = $this->languagesProgress;
 
+        // Get stats
         $data['perc'] = round(($data['languagesProgress'][$data['locale']] * 100) /
             $data['languagesProgress'][$data['fallback_locale']]);
 
-        return array('data' => $data);
+        return ['data' => $data];
     }
 
-    private function makeI18nData()
-    {
-        $this->setLanguagesList();
-        $this->setLanguagesData();
-    }
-
+    /**
+     * Get all the data needed for an AJAX call.
+     *
+     * @return array
+     */
     public function retrieveI18nData()
     {
+        // Initialise lari18n data.
         $this->makeI18nData();
 
-        $data = array();
+        $data = [];
 
         $data['paths'] = $this->paths;
         $data['languages'] = $this->languagesList;
@@ -290,6 +296,17 @@ class Lari18n
     }
 
     /**
+     * Get all the needed data for lari18n.
+     */
+    private function makeI18nData()
+    {
+        $this->setLanguagesList();
+        $this->setLanguagesData();
+    }
+
+    /**
+     * Reinitialise a translation file.
+     *
      * @param $files
      * @param $locale
      */
@@ -297,15 +314,17 @@ class Lari18n
     {
         foreach ($files as $file) {
 
-            $fileName = str_replace(array('.php', '\\'), array('', '/'), $file->getRelativePathname());
-            $translations = Lang::get($fileName, array(), $locale);
+            $fileName = str_replace(['.php', '\\'], ['', '/'], $file->getRelativePathname());
+            $translations = Lang::get($fileName, [], $locale);
 
-            array_walk_recursive($translations, array($this, 'reinitialiseRecursiveWalk'));
+            array_walk_recursive($translations, [$this, 'reinitialiseRecursiveWalk']);
             File::put($file->getPathName(), '<?php' . "\r\n\r\n" . 'return ' . var_export($translations, true) . ';');
         }
     }
 
     /**
+     * Recursive localisation item reinitialisation.
+     *
      * @param $item
      * @param $key
      */
@@ -326,6 +345,7 @@ class Lari18n
      */
     public function translate($fallback_locale, $locale, $key, $value)
     {
+        // Initialise lari18n AJAX data.
         $this->retrieveI18nData();
 
         array_set($this->languagesData[$locale], $key, $value);
@@ -355,12 +375,14 @@ class Lari18n
     {
         $this->translate($fallback_locale, $locale, $key, $value);
 
-        $replace = $replace ?: array();
+        $replace = $replace ?: [];
 
         echo $this->translator->choice($key, $number, $replace, $locale, false);
     }
 
     /**
+     * Return the lari18n activation state.
+     *
      * @return boolean
      */
     public static function isActivated()
@@ -368,6 +390,9 @@ class Lari18n
         return self::$activated;
     }
 
+    /**
+     * Activate lari18n.
+     */
     public static function activate()
     {
         self::$activated = true;
